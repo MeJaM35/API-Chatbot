a665be25891c3f65c2aa9dc393efe8fa28d0bd44
Meet Jamsutkar <jamsutkarmeetpradeep@gmail.com>
2024-03-25 10:51:33 +0530

    Chatbot: Installing Dependencies

--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from openai import OpenAI
+
+
+client = OpenAI(
+    # Defaults to os.environ.get("OPENAI_API_KEY")
+    api_key='your key here'
+)
+
+
+
 
 
 def index(request):
